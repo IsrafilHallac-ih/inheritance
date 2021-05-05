1638c58ed28e0e0adb435d0964b166ce33dbc9fe
IsrafilHallac-ih <83336393+IsrafilHallac-ih@users.noreply.github.com>
2021-05-05 21:14:50 +0300

    lesson3

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,28 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		IndividualCustomer individualCustomer=new IndividualCustomer();
+		individualCustomer.customerNumber ="12345";
+		
+		
+		
+		CorporateCustomer corporateCustomer= new CorporateCustomer();
+		corporateCustomer.customerNumber ="45697";
+		
+		CustomerManager customerManager=new CustomerManager();
+		Customer [] customers= {individualCustomer,corporateCustomer};
+		
+		customerManager.addMultiple(customers);
+		
+		
+
+	
+		
+		
+		
+
+	}
+
+}
